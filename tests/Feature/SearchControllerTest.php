40d0434edab6ddf41a0eb21467d5625c51e67218
Astiel Zeppeli <astiel@artemis.localdomain>
2022-06-02 20:21:43 +0000

    test verificar searchcontroller

---
+++ tests/Feature/SearchControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SearchControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_invalid_values_search()
+    {
+        $response = $this->get("api/search/0/0");
+        $response->assertStatus(404);
+
+        $response = $this->get("api/search/1654041600/0");
+        $response->assertStatus(404);
+
+        $response = $this->get("api/search/1654041600/-100");
+        $response->assertStatus(404);
+
+        $response = $this->get("api/search/0/texto");
+        $response->assertStatus(500);
+
+        $response = $this->get("api/search/texto/0");
+        $response->assertStatus(500);
+    }
+
+    public function test_valid_post_search()
+    {
+        $response = $this->get("/api/search/100000000000/10");
+        $response->assertStatus(200);
+
+        $response = $this->get("/api/search/100000000000/0");
+        $response->assertStatus(200);
+    }
+}
